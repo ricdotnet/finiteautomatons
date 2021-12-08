3f771b272660ef4c2bf33e1856f17367762945ca
Ricardo Rocha <me@rrocha.uk>
2021-12-08 12:50:59 +0000

    * updates;

--- src/dev/ricr/NfaParallel.java
+++ src/dev/ricr/NfaParallel.java
@@ -1,14 +1,11 @@
 package dev.ricr;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class NfaParallel {
 
-  private int stateSet;
-  public void reset() {
-    stateSet = 1<<0; // this value (1) represents the initial state set {q0}
+  private int currentState;
+
+  public void reset () {
+    currentState = 1 << 0; // this value (1) represents the initial state set {q0}
   }
 
   /**
@@ -16,47 +13,54 @@ public class NfaParallel {
    * The transition starts at delta[state,pattern.charAt()]
    */
   private int[][] delta =
-      {{1 << 0, 1<<0|1<<1},        // delta[q0, E,L,O,X] = {q0},  delta[q0, G] = {q0,q1}
+      {{1 << 0, 1 << 0 | 1 << 1},        // delta[q0, E,L,O,X] = {q0},  delta[q0, G] = {q0,q1}
           {0, 1 << 2},              // delta[q1, O] = {q1},  delta[q1, G] = {q2}
           {1 << 3, 0},                // delta[q2, L] = {q3}
           {1 << 4, 0}                 // delta[q3, E] = {q4}
       };
 
-  public boolean acceptsWord(String in) {
+//  char[] sequence = "GOGLE".toCharArray();
+//  public boolean acceptsWord(String w) {
+//    for (int i = 0; i < sequence.length; i++) {
+//
+//      if (currentState == 1 << 4) return true;
+//
+//      char nextChar = w.charAt(i);
+//
+//      if (nextChar == sequence[currentState]) {
+//        currentState = delta[currentState][];
+//      }
+//
+//
+//    }
+//  }
+
+  public boolean acceptsWord (String in) {
     for (int i = 0; i < in.length(); i++) {
 
-      if (stateSet == 1<<4) return true; // reaching final state, quit!!!
+      if (currentState == 1 << 4) return true; // reaching final state, quit!!!
 
       char c = in.charAt(i);
       int nextSS = 0; // next state set, initially empty
 
       for (int s = 0; s <= 3; s++) { // for each state s
-        if ((stateSet & (1<<s)) != 0) { // if maybe in s
+        if ((currentState & (1 << s)) != 0) { // if maybe in s
           try {
-            if (c == 'G' && (stateSet == 1<<0)) {
+            if (c == 'G' && (currentState == 1 << 0)) {
               nextSS |= delta[0][1];
-            } else
-            if (c == 'G' && (stateSet == (1<<0|1<<1) || stateSet == 1<<2)) {
+            } else if (c == 'G' && (currentState == (1 << 0 | 1 << 1) || currentState == 1 << 2)) {
               nextSS |= delta[1][1];
-            } else
-
-            if (c == 'O' && (stateSet == (1<<0|1<<1)) /*1*/) {
+            } else if (c == 'O' && (currentState == (1 << 0 | 1 << 1)) /*1*/) {
 //              nextSS |= delta[s][0];
-            } else
-
-            if (c == 'L' && stateSet == 1<<2) {
+            } else if (c == 'L' && currentState == 1 << 2) {
               nextSS |= delta[s][0];
-            } else
-
-            if (c == 'E' && stateSet == 1<<3) {
+            } else if (c == 'E' && currentState == 1 << 3) {
               nextSS |= delta[s][0];
 
-            } else if (c == 'O' && (stateSet == (1<<2))) { // reset back 1 state
+            } else if (c == 'O' && (currentState == (1 << 2))) { // reset back 1 state
               nextSS |= delta[0][1];
-              stateSet = nextSS;
-            }
-
-            else {
+              currentState = nextSS;
+            } else {
               nextSS |= delta[0][0];
               reset();
             }
@@ -69,10 +73,10 @@ public class NfaParallel {
       }
 
       if (c != 'O')
-        stateSet = nextSS; // new state set after c
+        currentState = nextSS; // new state set after c
     }
 
-    return (stateSet & (1<<4)) != 0;
+    return (currentState & (1 << 4)) != 0;
   }
 
   /**
@@ -83,16 +87,5 @@ public class NfaParallel {
     this.reset();
     return acceptsWord(w) ? "valid" : "not valid";
   }
-
-//  public static void main (String[] args) throws IOException {
-//    NfaParallel main = new NfaParallel();
-//
-//    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-//    String w = in.readLine();
-//
-//    main.reset();
-//    main.acceptsNext(w);
-//    System.out.println(main.checkWord());
-//  }
 
 }
--- src/dev/ricr/Run.java
+++ src/dev/ricr/Run.java
@@ -72,8 +72,8 @@ public class Run {
         String w = scanner.nextLine();
 //        String result = dfaTable.checkWord(w);
 //        String result = dfaIf.checkWord(w);
-//        String result = nfaParallel.checkWord(w);
-        String result = nfa.checkWord(w);
+        String result = nfaParallel.checkWord(w);
+//        String result = nfa.checkWord(w);
 
         System.out.println(w + ": " + result);
       }
